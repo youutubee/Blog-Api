e1d452968a71fdaa6e1f2ab54bdf1fa4d0aa8b32
youutubee <justforyouutubee@gmail.com>
2025-09-15 19:46:19 +0530

    Created more routers for the login and writing blog functions

--- Router/router.go
+++ Router/router.go
@@ -8,5 +8,7 @@ import (
 func Router() *mux.Router {
 	r := mux.NewRouter()
 	r.HandleFunc("/signup", controller.CreateUser).Methods("POST")
+	r.HandleFunc("/login", controller.LoginOneUser).Methods("POST")
+	r.HandleFunc("/insertblog", controller.InsertOneBlog).Methods("POST")
 	return r
 }
